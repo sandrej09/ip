74b680b1155e0bf24fcf01fbb054ad68b2e411c7
sandrej09 <makg.pacz@gmail.com>
2025-08-28 17:20:23 +0800

    Level-3: Mark & Done functions

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        isDone = true;
+    }
+
+    public void unmark() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String status = isDone ? "[X]" : "[ ]";
+        return status + " " + description;
+    }
+}

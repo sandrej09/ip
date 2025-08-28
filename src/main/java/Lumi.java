ccf15a082818f483cc5618f1650b4334a66141d7
sandrej09 <makg.pacz@gmail.com>
2025-08-28 17:20:23 +0800

    Level-3: Add & List & Mark & Done functions

--- src/main/java/Lumi.java
+++ src/main/java/Lumi.java
@@ -1,10 +1,54 @@
+import java.util.Scanner;
+
 public class Lumi {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        Task[] tasks = new Task[100];
+        int count = 0;
+
+        System.out.println(LINE);
         System.out.println(" Hello! I'm Lumi");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+
+        while (true) {
+            String input = sc.nextLine().trim();
+
+            if (input.equals("bye")) {
+                System.out.println(LINE);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            } else if (input.equals("list")) {
+                System.out.println(LINE);
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < count; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(LINE);
+            } else if (input.startsWith("mark ")) {
+                int idx = Integer.parseInt(input.substring(5)) - 1;
+                tasks[idx].mark();
+                System.out.println(LINE);
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[idx]);
+                System.out.println(LINE);
+            } else if (input.startsWith("unmark ")) {
+                int idx = Integer.parseInt(input.substring(7)) - 1;
+                tasks[idx].unmark();
+                System.out.println(LINE);
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[idx]);
+                System.out.println(LINE);
+            } else {
+                tasks[count] = new Task(input);
+                count++;
+                System.out.println(LINE);
+                System.out.println(" added: " + input);
+                System.out.println(LINE);
+            }
+        }
     }
 }
